52c3d9040b1f52688291f32e7273ae155f62e60b
sundarCH <sundarCH@LAPTOP-DFKUOIM5>
2024-09-22 00:29:20 +0530

    test Push in git

--- Documents/microService/CloudGateway/src/main/java/com/sundar/CloudGatewayApplication.java
+++ Documents/microService/CloudGateway/src/main/java/com/sundar/CloudGatewayApplication.java
@@ -10,6 +10,7 @@ public class CloudGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudGatewayApplication.class, args);
+		System.out.println("Cool");
 	}
 
 }
