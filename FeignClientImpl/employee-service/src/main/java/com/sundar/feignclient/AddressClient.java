61e6b3868e2f14d2b886694e9395ca16b848be16
sundarCH <sundarCH@LAPTOP-DFKUOIM5>
2024-10-05 16:28:09 +0530

    comment on application.properties file

--- FeignClientImpl/employee-service/src/main/java/com/sundar/feignclient/AddressClient.java
+++ FeignClientImpl/employee-service/src/main/java/com/sundar/feignclient/AddressClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.sundar.pojo.AddressResponse;
 
-@FeignClient(name = "address-service", url = "http://localhost:8082", path = "/address-service")
+@FeignClient(name = "address-service", url = "http://localhost:8082/address")
 public interface AddressClient {
 	
 	@GetMapping("/address/{id}")
